a145ff2bdf1a8da4688a6a7d1cb1fb16b0e2d558
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-08 00:44:14 +0100

    feat: :sparkles: set base model and other child class

---
+++ super_admin_1/models/shop.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""Template for the Shop Class"""
+from super_admin_1 import db
+from super_admin_1.models.base import BaseModel
+from super_admin_1.models.enum import AdminStatus, Restricted
+
+
+class Shop(BaseModel):
+  """
+  """
+  __tablename__ = "shop"
+
+  merchand_id = db.Column(db.String(60), db.ForeignKey("user.id"), nullable=False)
+  product_id = db.Column(db.String(60), db.ForeignKey("product.id"), nullable=False)
+  name = db.Column(db.String(60), nullable=False)
+  policy_confimation = db.Column(db.Boolean, nullable=False)
+  restricted = db.Column(db.Enum('no', 'temporary', 'permanent', name="Restricted"), server_default="no" , nullable=False)
+  admin_status = db.Column(db.Enum('pending', 'review', 'approved', 'blacklist', name="AdminStatus"), server_default="pending", nullable=False,)
+  reviewed = db.Column(db.Boolean, nullable=True, default=False)
+  rating = db.Column(db.Numeric(10, 2), nullable=True, default=0)
+ 
+  
+  
+  def __init__(self, merchand_id, product_id, name, policy_confimation, restricted, admin_status, reviewed, rating):
+    """object constructor"""
+    self.merchand_id = merchand_id
+    self.product_id = product_id
+    self.name = name
+    self.policy_confimation = policy_confimation
+    self.restricted = restricted or "no"
+    self.admin_status = admin_status or "pending"
+    self.reviewed = reviewed 
+    self.rating = rating
+    
+  def __repr__(self):
+    """official object representation"""
+    return ({
+      "id": self.id,
+      "merchand_id": self.merchand_id,
+      "product_id": self.product_id,
+      "name": self.name,
+      "policy_confimation": self.policy_confimation,
+      "restricted": self.restricted,
+      "admin_status": self.admin_status,
+      "reviewed": self.reviewed,
+      "rating": self.rating,
+    })
+    
+  def format(self):
+    """Format the object's attributes as a dictionary"""
+    return ({
+      "id": self.id,
+      "merchand_id": self.merchand_id,
+      "product_id": self.product_id,
+      "name": self.name,
+      "policy_confimation": self.policy_confimation,
+      "restricted": self.restricted,
+      "admin_status": self.admin_status,
+      "reviewed": self.reviewed,
+      "rating": self.rating,
+    })
+    
+    
+
+  
