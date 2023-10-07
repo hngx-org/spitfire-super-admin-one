4e6d8620c74c1b39004a7b84526aa97cfdb7bb29
Freeman-kuch <freemanoparaoch@gmail.com>
2023-10-07 16:39:43 +0100

    initialized psycopg2 test connection

---
+++ super_admin_1/models/alternative.py
@@ -0,0 +1,81 @@
+import psycopg2
+from psycopg2 import sql
+
+# ++++++++++++++++++++++++++++++THIS IS A TEST SCENARIO!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class Database:
+    def __init__(self, dbname, user, password, host, port):
+        self.connection = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        self.cursor = self.connection.cursor()
+
+    def execute_query(self, query, params=None):
+        try:
+            if params:
+                self.cursor.execute(query, params)
+            else:
+                self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as error:
+            self.connection.rollback()
+            print("Error executing query:", error)
+
+    def fetch_data(self, query, params=None):
+        try:
+            if params:
+                self.cursor.execute(query, params)
+            else:
+                self.cursor.execute(query)
+            return self.cursor.fetchall()
+        except Exception as error:
+            print("Error fetching data:", error)
+
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
+
+# Example usage:
+if __name__ == "__main__":
+    # Database configuration
+    host = "dpg-ckfhkk0l3its73c4s3ag-a.oregon-postgres.render.com"
+    dbname = "zuri_marketplace"
+    user = "zuri_marketplace"
+    port = 5432
+    password =  "L6j7HTs3B0aoJqGLxHALSEamxFs1rd0m"
+
+    # Create a Database instance
+    db = Database(dbname, user, password, host, port)
+
+    # Example queries
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255),
+        email VARCHAR(255)
+    )
+    """
+
+    insert_data_query = """
+    INSERT INTO users (username, email) VALUES (%s, %s)
+    """
+
+    select_data_query = "SELECT * FROM public.shop"
+
+    # Execute queries
+    # db.execute_query(create_table_query)
+    # db.execute_query(insert_data_query, ("john_doe", "john@example.com"))
+    # db.execute_query(insert_data_query, ("jane_smith", "jane@example.com"))
+
+    # Fetch and print data
+    shop = db.fetch_data(select_data_query)
+    print("shop:")
+    print(shop)
+    for user in shop:
+        print(user)
+
+    # Close the database connection
+    db.close_connection()
