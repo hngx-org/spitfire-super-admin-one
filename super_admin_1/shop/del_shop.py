f6a88926cf82f062b9de97f74f6861a3eced364b
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-08 15:51:25 +0100

    feat: :sparkles: commented helper function for code consistencies

--- super_admin_1/shop/del_shop.py
+++ super_admin_1/shop/del_shop.py
@@ -19,7 +19,7 @@ def delete_shop(shop_id):
   shop = Shop.query.filter_by(id=shop_id).first()
   if not shop:
     return jsonify({'forbidden': 'Shop not found'}), 404
-  # check if shop is temporary
+  # check if shop is already temporary deleted
   if shop.is_deleted == 'temporary':
     return jsonify({'message': 'Shop already deleted'}), 400
   # delete shop temporarily
@@ -28,100 +28,105 @@ def delete_shop(shop_id):
   return jsonify({'message': 'Shop temporarily deleted'}), 200
 
 
-@del_shop.route("/user/create", methods=["POST"])
-def create_user():
-    """Create a new user"""
-    if not request.get_json():
-        abort(400)
-    data_fields = [
-        "username",
-        "first_name",
-        "last_name",
-        "email",
-        "section_order",
-        "password",
-        "is_verified",
-        "two_factor_auth",
-        "provider",
-        "profile_pic",
-        "refresh_token",
-    ]
-    for field in data_fields:
-        if field not in request.get_json():
-            abort(400)
-    user = User(**request.get_json())
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.format()), 201
+# ====================================== Helper Function ===========================================
+
+# @del_shop.route("/user/create", methods=["POST"])
+# def create_user():
+#     """Create a new user"""
+#     if not request.get_json():
+#         abort(400)
+#     data_fields = [
+#         "username",
+#         "first_name",
+#         "last_name",
+#         "email",
+#         "section_order",
+#         "password",
+#         "is_verified",
+#         "two_factor_auth",
+#         "provider",
+#         "profile_pic",
+#         "refresh_token",
+#     ]
+#     for field in data_fields:
+#         if field not in request.get_json():
+#             abort(400)
+#     user = User(**request.get_json())
+#     db.session.add(user)
+#     db.session.commit()
+#     return jsonify(user.format()), 201
 
 
-@del_shop.route("/user/<user_id>/shop", methods=["POST"])
-def create_shop(user_id):
-    """Create a new shop"""
-    if not request.get_json():
-        abort(400)
-    data_fields = ["name", "policy_confimation", "reviewed", "rating"]
-    for field in data_fields:
-        if field not in request.get_json():
-            abort(400)
-        else:
-            continue
-    data = request.get_json()
-    data["merchant_id"] = user_id
-    shop = Shop(**data)
-    db.session.add(shop)
-    db.session.commit()
-    return jsonify(shop.format()), 201
+# @del_shop.route("/user/<user_id>/shop", methods=["POST"])
+# def create_shop(user_id):
+#     """Create a new shop"""
+#     if not request.get_json():
+#         abort(400)
+#     data_fields = ["name", "policy_confimation", "reviewed", "rating"]
+#     for field in data_fields:
+#         if field not in request.get_json():
+#             abort(400)
+#         else:
+#             continue
+#     data = request.get_json()
+#     data["merchant_id"] = user_id
+#     shop = Shop(**data)
+#     db.session.add(shop)
+#     db.session.commit()
+#     return jsonify(shop.format()), 201
 
 
-# get request for shop
-@del_shop.route(
-    "/shop", methods=["GET"], strict_slashes=False, defaults={"shop_id": None}
-)
-@del_shop.route("/shop/<shop_id>", methods=["GET"])
-def get_shop(shop_id):
-    """Get a shop or all shop"""
-    if shop_id:
-        return jsonify(Shop.query.filter_by(id=shop_id).first().format()), 200
-    else:
-        return jsonify([shop.format() for shop in Shop.query.all()]), 200
+# # get request for shop
+# @del_shop.route(
+#     "/shop", methods=["GET"], strict_slashes=False, defaults={"shop_id": None}
+# )
+# @del_shop.route("/shop/<shop_id>", methods=["GET"])
+# def get_shop(shop_id):
+#     """Get a shop or all shop"""
+#     if shop_id:
+#         return jsonify(Shop.query.filter_by(id=shop_id).first().format()), 200
+#     else:
+#         return jsonify([shop.format() for shop in Shop.query.all()]), 200
 
 
-# delete shop object
-# @del_shop.route('/shop/<shop_id>', methods=['DELETE'])
-# def perm_del(shop_id):
-#   """ Delete a shop"""
-#   shop = Shop.query.filter_by(id=shop_id).first()
-#   if not shop:
-#     abort(404)
-#   db.session.delete(shop)
-#   db.session.commit()
-#   return jsonify({'message': 'Shop deleted'}), 200
+# # delete shop object
+# # @del_shop.route('/shop/<shop_id>', methods=['DELETE'])
+# # def perm_del(shop_id):
+# #   """ Delete a shop"""
+# #   shop = Shop.query.filter_by(id=shop_id).first()
+# #   if not shop:
+# #     abort(404)
+# #   db.session.delete(shop)
+# #   db.session.commit()
+# #   return jsonify({'message': 'Shop deleted'}), 200
 
 
-# get request for user
-@del_shop.route(
-    "/user", methods=["GET"], strict_slashes=False, defaults={"user_id": None}
-)
-@del_shop.route("/user/<user_id>", methods=["GET"])
-def get_user(user_id):
-    """Get all user"""
-    if user_id:
-        return jsonify(User.query.filter_by(id=user_id).first().format()), 200
-    else:
-        return jsonify([user.format() for user in User.query.all()]), 200
+# # get request for user
+# @del_shop.route(
+#     "/user", methods=["GET"], strict_slashes=False, defaults={"user_id": None}
+# )
+# @del_shop.route("/user/<user_id>", methods=["GET"])
+# def get_user(user_id):
+#     """Get all user"""
+#     if user_id:
+#         return jsonify(User.query.filter_by(id=user_id).first().format()), 200
+#     else:
+#         return jsonify([user.format() for user in User.query.all()]), 200
 
 
-# delete user object
-@del_shop.route("/user/<user_id>", methods=["DELETE"])
-def delete_user(user_id):
-    """Delete a user"""
-    user = User.query.filter_by(id=user_id).first()
-    if not user:
-        abort(404)
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({"message": "User deleted"}), 200
+# # delete user object
+# @del_shop.route("/user/<user_id>", methods=["DELETE"])
+# def delete_user(user_id):
+#     """Delete a user"""
+#     user = User.query.filter_by(id=user_id).first()
+#     if not user:
+#         abort(404)
+#     db.session.delete(user)
+#     db.session.commit()
+#     return jsonify({"message": "User deleted"}), 200
+# ============================= End Helper Function ===========================================
+
+
 
 
 @del_shop.route("/logs/shops", defaults={"shop_id": None})
