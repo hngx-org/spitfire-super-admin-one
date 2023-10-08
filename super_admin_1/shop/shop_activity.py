3626989f649b336b7fd1e6ce922bb51526ebb5d4
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-10-08 14:03:43 +0100

    Update the blueprint

--- super_admin_1/shop/shop_activity.py
+++ super_admin_1/shop/shop_activity.py
@@ -3,19 +3,10 @@ from flask import Blueprint, jsonify
 from super_admin_1.models.shop_log import ShopLog
 
 
-shop_activity = Blueprint('shop_activity', __name__)
+events = Blueprint("events", __name__, url_prefix="/api/events")
 
 
-@shop_activity.route('/shop/actions', methods=['GET'])
+@events.route('/shop/actions', methods=['GET'])
 def shop_actions():
     data = ShopLog.query.all()
     return jsonify([action.format() for action in data]), 200
-
-
-# The following lines of code needs to by added to actions that needs to be logged
-# get_user_id = shop.user.id
-# notice = {"user_id": get_user_id,
-#             "action": "delete shop", "shop_id": shop_id}
-
-# activity = ShopLog(**notice)
-# activity.insert()
