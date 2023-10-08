6b48e3659646264b49a0163ab5b8beb8a6d25216
Holic65 <56598437+Holic65@users.noreply.github.com>
2023-10-08 18:23:19 +0100

    Removed id from init
    Signed-off-by: Holic65 <56598437+Holic65@users.noreply.github.com>

--- super_admin_1/models/user_product_rating.py
+++ super_admin_1/models/user_product_rating.py
@@ -8,7 +8,6 @@ from super_admin_1.models.base import BaseModel
 class User_Product_Rating(BaseModel):
     """User_Product_Rating class"""
     __tablename__ = "user_product_rating"
-    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     user_id = db.Column(db.String(60), db.ForeignKey("user.id"), nullable=False)
     product_id = db.Column(db.String(60), db.ForeignKey("product.id"), nullable=False)
     rating = db.Column(db.Numeric(5, 2), nullable=False, default=0)
