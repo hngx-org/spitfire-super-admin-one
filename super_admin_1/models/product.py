b78875347cc6a8a86d0ab3f979401402385f356d
Holic65 <okormark@gmail.com>
2023-10-08 12:10:13 +0100

    removed image column

--- super_admin_1/models/product.py
+++ super_admin_1/models/product.py
@@ -9,8 +9,7 @@ class Product(BaseModel):
   """ Product class"""
   __tablename__ = "product"  
   shop_id = db.Column(db.String(60), db.ForeignKey("shop.id"), nullable=False) 
-  rating_id = db.Column(db.String(60), db.ForeignKey("user_product_rating.id"), nullable=False)
-  image_id = db.Column(db.String(60), db.ForeignKey("product_image.id"), nullable=False)
+  rating_id = db.Column(db.String(60), db.ForeignKey("user_product_rating.id"))
   category_id = db.Column(db.String(60), db.ForeignKey("product_category.id"), nullable=False)  
   name = db.Column(db.String(32), nullable=False)
   description = db.Column(db.String(512), nullable=False)
@@ -23,12 +22,11 @@ class Product(BaseModel):
   is_published = db.Column(db.Boolean, nullable=False, default=False)
   currency = db.Column(db.String(16), nullable=False)
 
-  def __init__(self, shop_id, rating_id, image_id, category_id, name, description, quantity, price, discount_price, tax, admin_status, is_deleted, is_published, currency):
+  def __init__(self, shop_id, rating_id, category_id, name, description, quantity, price, discount_price, tax, admin_status, is_deleted, is_published, currency):
     """ object constructor"""
     super().__init__()
     self.shop_id = shop_id
     self.rating_id = rating_id
-    self.image_id = image_id
     self.category_id = category_id
     self.name = name
     self.description = description
@@ -44,7 +42,7 @@ class Product(BaseModel):
     
   def __repr__(self):
     """ official object representation"""
-    return f"(id: {self.id}, shop_id: {self.shop_id}, rating_id: {self.rating_id}, image_id: {self.image_id}, category_id: {self.category_id}, name: {self.name}, description: {self.description}, quantity: {self.quantity}, price: {self.price}, discount_price: {self.discount_price}, tax: {self.tax}, admin_status: {self.admin_status}, is_published: {self.is_published}, is_deleted: {self.is_deleted}, currency: {self.currency}, created_at: {self.created_at}, updated_at: {self.updated_at})"
+    return f"(id: {self.id}, shop_id: {self.shop_id}, rating_id: {self.rating_id}, category_id: {self.category_id}, name: {self.name}, description: {self.description}, quantity: {self.quantity}, price: {self.price}, discount_price: {self.discount_price}, tax: {self.tax}, admin_status: {self.admin_status}, is_published: {self.is_published}, is_deleted: {self.is_deleted}, currency: {self.currency}, created_at: {self.created_at}, updated_at: {self.updated_at})"
     
   def format(self):
     """Format the object's attributes as a dictionary"""
@@ -52,7 +50,6 @@ class Product(BaseModel):
       "id": self.id,
       "shop_id": self.shop_id,
       "rating_id": self.rating_id,
-      "image_id": self.image_id,
       "category_id": self.category_id,
       "name": self.name,
       "description": self.description,
