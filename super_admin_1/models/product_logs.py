2f37906c1392e6d730edad348336ec6f7190512c
Holic65 <okormark@gmail.com>
2023-10-08 11:19:28 +0100

    product log and shop log

--- super_admin_1/models/product_logs.py
+++ super_admin_1/models/product_logs.py
@@ -3,28 +3,35 @@
 
 from super_admin_1 import db
 from super_admin_1.models.base import BaseModel
-
+from datetime import datetime
 
 class Product_Logs(BaseModel):
     """Product_Image class"""
     __tablename__ = "product_logs"
     user_id = db.Column(db.String(60), db.ForeignKey("user.id"), nullable=False)
+    action = db.Column(db.String(20), nullable=False)
     product_id = db.Column(db.String(60), db.ForeignKey("product.id"), nullable=False)
     log_date = db.Column(db.TIMESTAMP, default=db.func.current_timestamp(), nullable=False)
     
-    def __init__(self, product_id, url):
+    def __init__(self,user_id, action, product_id, log_date=None):
         """Object constructor"""
         super().__init__()
+        self.user_id = user_id
+        self.action = action
         self.product_id = product_id
-        self.url = url
+        if log_date is None:
+            log_date = datetime.utcnow()
+        self.log_date = log_date
 
     def __repr__(self):
         """official object representation"""
-        return f"(product_id: {self.product_id}, url: {self.url})"
+        return f"(user_id: {self.user_id}, action: {self.action}, product_id: {self.product_id}, log_date: {self.log_date})"
     
     def format(self):
         """Format the object's attributes as a dictionary"""
         return ({
+            "user_id": self.user_id,
+            "action": self.action,
             "product_id": self.product_id,
-            "url": self.url
+            "log_date": self.log_date
         })
--- super_admin_1/models/shop_logs.py
+++ super_admin_1/models/shop_logs.py
@@ -3,28 +3,35 @@
 
 from super_admin_1 import db
 from super_admin_1.models.base import BaseModel
-
+from datetime import datetime
 
 class Shop_Logs(BaseModel):
     """Shop_Log class"""
     __tablename__ = "product_logs"
     user_id = db.Column(db.String(60), db.ForeignKey("user.id"), nullable=False)
+    action = db.Column(db.String(20), nullable=False)
     shop_id = db.Column(db.String(60), db.ForeignKey("shop.id"), nullable=False)
     log_date = db.Column(db.TIMESTAMP, default=db.func.current_timestamp(), nullable=False)
     
-    def __init__(self, shop_id, url):
+    def __init__(self, user_id, action, shop_id, log_date=None):
         """Object constructor"""
         super().__init__()
+        self.user_id = user_id
+        self.action = action
         self.shop_id = shop_id
-        self.url = url
+        if log_date is None:
+            log_date = datetime.utcnow()
+        self.log_date = log_date
 
     def __repr__(self):
         """official object representation"""
-        return f"(shop_id: {self.shop_id}, url: {self.url})"
+        return f"(user_id: {self.user_id}, action: {self.action}, shop_id: {self.shop_id}, log_date: {self.log_date})"
     
     def format(self):
         """Format the object's attributes as a dictionary"""
         return ({
+            "user_id": self.user_id,
+            "action": self.action,
             "shop_id": self.shop_id,
-            "url": self.url
-        })￼Enter
+            "log_date": self.log_date
+        })
