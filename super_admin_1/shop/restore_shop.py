a20c6c64ff9841497547f6228720b37c175a6016
Murad <83445039+Murad039@users.noreply.github.com>
2023-10-09 11:17:09 +0600

    Update restore_shop.py
    fixed some typo
    
    Signed-off-by: Murad <83445039+Murad039@users.noreply.github.com>

--- super_admin_1/shop/restore_shop.py
+++ super_admin_1/shop/restore_shop.py
@@ -3,12 +3,11 @@
 from super_admin_1 import db
 from flask import Blueprint, jsonify, request, abort
 from super_admin_1.models.shop import Shop
-from super_admin_1.models import User
 
-restore_shop = Blueprint("restore_shop", __name__)
+restore_shop = Blueprint("restore_shop", __name__, url_prefix='/api/restore_shop')
 
 
-@restore_shop.route("/shop/<shop_id>", methods=["PATCH"])
+@restore_shop.route("/<shop_id>", methods=["PATCH"])
 def restore_shop(shop_id):
     """restores a deleted shop by setting their "temporary" to "active" fields
     Args:
@@ -18,15 +17,15 @@ def restore_shop(shop_id):
         -success(HTTP 200):shop restored successfully
         -success(HTTP 200): if the shop with provided not marked as deleted
     """
-    data = request.get_json()
-    if not request.is_json:
-        abort(400), "JSON data required"
-    shop = shop.query.filter_by(id=shop_id).first()
+   # data = request.get_json()
+    #if not request.is_json:
+        #abort(400), "JSON data required"
+    shop = Shop.query.filter_by(id=shop_id).first()
     if not shop:
         abort(404), "Invalid shop"
     # change the object attribute from temporary to active
-    if shop.is_deleted == "temporary":
-        shop.is_deleted = "active"
+    if shop.is_deleted == 'temporary':
+        shop.is_deleted = 'active'
         db.session.commit()
         return jsonify({"message": "shop restored sufccessfully"}), 200
     else:
