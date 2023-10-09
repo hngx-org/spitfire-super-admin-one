e1c5435d9e2800e983972106847ce1f32790bdaf
Dev-wonderful <adeonderful20@gmail.com>
2023-10-09 12:42:07 +0100

    Log modification

--- super_admin_1/products/event_logger.py
+++ super_admin_1/products/event_logger.py
@@ -1,15 +1,16 @@
+import os
 import logging
 from datetime import date
 from super_admin_1 import db
 from super_admin_1.models.alternative import Database
 from super_admin_1.models.product_logs import ProductLogs
 
-
+log_file_name=f'product_log_report_{date.today().strftime("%Y_%m_%d")}.log'
 # Configure the logging module
 logging.basicConfig(
-    filename=f'product_log_report_{date.today().strftime("%Y_%m_%d")}.txt',
-    level=logging.INFO,
-    format="%(levelname)s - %(message)s",
+    filename=f'server_logs_{date.today().strftime("%Y_%m_%d")}.log',
+    level=logging.FATAL,
+    format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
 
@@ -20,7 +21,7 @@ def generate_log_file():
         log_message = f"Admin '{log.user_id}' performed action: '{log.action}'\
               on product with Id '{log.product_id}' time: {log.log_date}"
         logging.info(log_message)
-    return f'admin_actions_{date.today().strftime("%Y_%m_%d")}.txt'
+    return log_file_name
 
 
 def register_action(user_id, action, product_id):
@@ -39,15 +40,20 @@ def generate_log_file_d():
             cursor.execute(query)
             all_logs = cursor.fetchall()
             if all_logs == None:
-                return {
-                    "message": "No logs available"
-                }
+                return False
+            if os.path.isfile(log_file_name):
+                os.remove(log_file_name)
             for log in all_logs:
-                print(f"type: {type(log)}")
+                # print(f"type: {type(log)}")
                 # log = id , user_id, action, product_id
-                log_message = f" Admin '{log[1]}' performed action: '{log[2]}' on product with Id '{log[3]}'"
-                logging.info(log_message)
-        return f'admin_actions_{date.today().strftime("%Y_%m_%d")}.txt'
+                log_message = f" Admin '{log[1]}' performed action: '{log[2]}' on product with Id '{log[3]}'\n"
+                try:
+                    with open(log_file_name, 'a') as log_file:
+                        log_file.write(log_message)
+                except Exception:
+                    return False
+                # logging.info(log_message)
+        return log_file_name
     except Exception as error:
         print(f"{type(error).__name__}: {error}")
 
@@ -60,7 +66,6 @@ def register_action_d(user_id, action, product_id):
             VALUES (%s, %s, %s);
         """
         with Database() as cursor:
-            insert_response = cursor.execute(query, (user_id, action, product_id))
-            # print(insert_response)
+            cursor.execute(query, (user_id, action, product_id))
     except Exception as error:
         print(f"{type(error).__name__}: {error}")
