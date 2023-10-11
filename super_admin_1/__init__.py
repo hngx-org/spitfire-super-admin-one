16ea33c1a6665971999a8d078697957fcd83ecaf
Kevin Koech <kevinkoech@proton.me>
2023-10-11 17:54:35 +0300

    [Fix]: Resolve merge conflicts

--- super_admin_1/__init__.py
+++ super_admin_1/__init__.py
@@ -11,6 +11,7 @@ db = SQLAlchemy()
 # Create an instance of Swagger
 swagger = Swagger()
 
+
 def create_app():
     """
     Create a new instance of the app with the given configuration.
@@ -29,7 +30,7 @@ def create_app():
     CORS(app, supports_credentials=True)
 
     # Load Swagger content from the file
-    with open('swagger_config.yaml', 'r') as file:
+    with open("swagger_config.yaml", "r") as file:
         swagger_config = yaml.load(file, Loader=yaml.FullLoader)
     # Initialize Flasgger with the loaded Swagger configuration
     Swagger(app, template=swagger_config)
@@ -40,24 +41,14 @@ def create_app():
     # imports blueprints
     from super_admin_1.shop.routes import shop, logs
     from super_admin_1.products.routes import product
-<<<<<<< HEAD
-    from super_admin_1.shop.temp_deleted_vendors import deleted_vendors
-=======
     from super_admin_1.shop.func_helpers import test
-
->>>>>>> 259c32c056a31f0567795f3039f6e54807f07c85
 
     # register blueprint
     app.register_blueprint(shop)
     app.register_blueprint(logs)
     app.register_blueprint(product)
-<<<<<<< HEAD
-    app.register_blueprint(deleted_vendors)
-=======
     app.register_blueprint(test)
->>>>>>> 259c32c056a31f0567795f3039f6e54807f07c85
 
-    
     # create db tables from models if not exists
     with app.app_context():
         db.create_all()
