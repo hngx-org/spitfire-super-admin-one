f80292bb9ff07b600e83c820493fb9c30205a2ac
Dev-wonderful <adeonderful20@gmail.com>
2023-10-13 03:02:18 +0100

    product notification

--- super_admin_1/notification/config.py
+++ super_admin_1/notification/config.py
@@ -1,3 +1,8 @@
 url_mapping = {
-    "unban": "/api/v1/store/suspension-lifted"
+    "ban": "/api/v1/store/suspended",
+    "unban": "/api/v1/store/suspension-lifted",
+    "shop deletion": "/api/v1/store/deleted",
+    "sanction": "/api/v1/product/suspended",
+    "unsanction": "/api/v1/product/unsuspended",
+    "product deletion": "/api/v1/product/deleted",
 }
--- super_admin_1/notification/notification_helper.py
+++ super_admin_1/notification/notification_helper.py
@@ -46,7 +46,6 @@ def notify(action: str, **kwargs: str) -> dict:
     
     Keyword Args:
         product_id (str): id of the product being acted on.
-        shop_id (str): id of the shop being acted on.
         reason (str): reason for the action.
     
     Returns:
@@ -59,22 +58,23 @@ def notify(action: str, **kwargs: str) -> dict:
     try:
         data: dict = {}
         # update the data dictionary with the available keys
-        if kwargs.get("product_id", None):
-            product_name, shop_id = get_field_value(field="name, shop_id", table="product",
-                                                    filter="id", value=kwargs.get("product_id"))
-            data["product_name"] = product_name
-            merchant_id = get_field_value(field="merchant_id", table="shop", filter="id", value=shop_id)
-        else:
-            store_name, merchant_id = get_field_value(field="name, merchant_id", table="shop",
-                                        filter="id", value=kwargs.get("shop_id"))
-            data["store_name"] = store_name
-        if kwargs.get("reason", None):
-            data["reason"] = kwargs.get("reason")
+        product_name, shop_id, description = get_field_value(field="name, shop_id, description", 
+                                                             table="product", filter="id", 
+                                                             value=kwargs.get("product_id"))
+        data["product_name"] = product_name
+        merchant_id = get_field_value(field="merchant_id", table="shop", filter="id", value=shop_id)
+
         # query the database to get the recipient email
         email, name = get_field_value(field="email, name", table="public.user",
                                       filter="id", value=merchant_id)
+        url = get_field_value(field="url", table="product_image", 
+                              filter="product_id", value=kwargs.get("product_id"))
         data["recipient"] = email
         data["name"] = name
+        data["violation"] = kwargs.get("reason", "Policy violation")
+        data["image_url"] = url
+        data["store_link"] = "not defined"
+        data["product_info"] = description
         
     except Exception as error:
         logger.error(f"{type(error).__name__}: {error}")
@@ -106,6 +106,77 @@ def notify(action: str, **kwargs: str) -> dict:
         },
         "error": False
     }
+
+# def notify(action: str, **kwargs: str) -> dict:
+#     """Notify of an action
+
+#     Args:
+#         user_id (str): id of the admin user.
+#         action (str): the action being taken on a shop or products.
+#         **kwargs (dict): A dictionary of keyword arguments.
+    
+#     Keyword Args:
+#         product_id (str): id of the product being acted on.
+#         shop_id (str): id of the shop being acted on.
+#         reason (str): reason for the action.
+    
+#     Returns:
+#         dict: To signify success, failure or error
+#             - success (bool): signifies email sent
+#             - data (dict): details of the successful mail sent
+#             - error (bool): signifies error during execution
+#     """
+#     email_request_base_url = "https://team-titan.mrprotocoll.me"
+#     try:
+#         data: dict = {}
+#         # update the data dictionary with the available keys
+#         if kwargs.get("product_id", None):
+#             product_name, shop_id = get_field_value(field="name, shop_id", table="product",
+#                                                     filter="id", value=kwargs.get("product_id"))
+#             data["product_name"] = product_name
+#             merchant_id = get_field_value(field="merchant_id", table="shop", filter="id", value=shop_id)
+#         else:
+#             store_name, merchant_id = get_field_value(field="name, merchant_id", table="shop",
+#                                         filter="id", value=kwargs.get("shop_id"))
+#             data["store_name"] = store_name
+            
+#         # query the database to get the recipient email
+#         email, name = get_field_value(field="email, name", table="public.user",
+#                                       filter="id", value=merchant_id)
+#         data["recipient"] = email
+#         data["name"] = name
+#         data["violation"] = kwargs.get("reason", "Policy violation")
+        
+#     except Exception as error:
+#         logger.error(f"{type(error).__name__}: {error}")
+
+#     try:
+#         endpoint = url_mapping.get(action)
+#         response = requests.post(f"{email_request_base_url}/{endpoint}", json=data)
+#         if response.status_code != 200:
+#             return {
+#                 "success": False,
+#                 "data": {},
+#                 "error": False
+#             }
+#     except Exception as error:
+#         logger.error(f"{type(error).__name__}: {error}")
+#         return {
+#             "success": False,
+#             "data": {},
+#             "error": True
+#         }
+
+#     return {
+#         "success": True,
+#         "data": {
+#             "recipient": email,
+#             "name": name,
+#             "action": action,
+#             "affected_object": product_name | store_name
+#         },
+#         "error": False
+#     }
 
 def notify_test(name: str, email: str, store_name: str) -> dict:
     
